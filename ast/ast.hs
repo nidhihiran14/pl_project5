79813740fc011a5c567430dac8f4bb371f9e4036
Eric Atkinson <eric.atkinson93@gmail.com>
2025-04-17 18:55:20 +0300

    Initial commit

---
+++ ast/ast.hs
@@ -0,0 +1,20 @@
+module Ast where
+
+data Expr = Plus Expr Expr | Minus Expr Expr | Times Expr Expr | Div Expr Expr
+    | Literal Float
+
+eval :: Expr -> Float
+-- Your code goes here
+
+eq :: Expr -> Expr -> Bool
+-- Your code goes here
+
+-- Should eval to "5.0"
+test1 = Plus (Literal 3.0) (Literal 2.0)
+
+-- Should eval to "3.5"
+test2 = Plus (Literal 3.0) (Div (Literal 1.0) (Literal 2.0))
+
+-- Should eval to "15.5"
+test3 = Plus (Times (Literal 3.0) (Literal 5.0)) (Div (Literal 1.0) (Literal 2.0))
+
---
+++ password/password.hs
@@ -0,0 +1,17 @@
+module Password where
+
+newtype PwdOp a = ???
+
+setPassword :: String -> PwdOp ()
+-- Your code here
+
+checkPassword :: String -> PwdOp Bool
+-- Your code here
+
+runPwdOp :: PwdOp a -> a
+-- Your code here
+
+
+
+
+
