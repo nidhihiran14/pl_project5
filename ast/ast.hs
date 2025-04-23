0819d9beaf437c88a3c2e28c7994f7836bf99734
NIDHI HIRAN <nidhihiran@Mac.lan>
2025-04-23 15:32:35 -0400

    First commit

--- ast/ast.hs
+++ ast/ast.hs
@@ -1,13 +1,21 @@
 module Ast where
 
-data Expr = Plus Expr Expr | Minus Expr Expr | Times Expr Expr | Div Expr Expr
-    | Literal Float
+data Expr = Plus Expr Expr 
+          | Minus Expr Expr 
+          | Times Expr Expr 
+          | Div Expr Expr
+          | Literal Float
+          deriving (Show)
 
 eval :: Expr -> Float
--- Your code goes here
+eval (Literal n)     = n
+eval (Plus e1 e2)    = eval e1 + eval e2
+eval (Minus e1 e2)   = eval e1 - eval e2
+eval (Times e1 e2)   = eval e1 * eval e2
+eval (Div e1 e2)     = eval e1 / eval e2
 
 eq :: Expr -> Expr -> Bool
--- Your code goes here
+eq e1 e2 = eval e1 == eval e2
 
 -- Should eval to "5.0"
 test1 = Plus (Literal 3.0) (Literal 2.0)
@@ -17,4 +25,3 @@ test2 = Plus (Literal 3.0) (Div (Literal 1.0) (Literal 2.0))
 
 -- Should eval to "15.5"
 test3 = Plus (Times (Literal 3.0) (Literal 5.0)) (Div (Literal 1.0) (Literal 2.0))
-
--- password/password.hs
+++ password/password.hs
@@ -1,17 +1,29 @@
 module Password where
 
-newtype PwdOp a = ???
+newtype PwdOp a = PwdOp { runPwdOp' :: String -> (a, String) }
+
+instance Functor PwdOp where
+    fmap f (PwdOp g) = PwdOp $ \s -> let (a, s') = g s in (f a, s')
+
+instance Applicative PwdOp where
+    pure a = PwdOp $ \s -> (a, s)
+    (PwdOp f) <*> (PwdOp g) = PwdOp $ \s -> 
+        let (fab, s1) = f s
+            (a, s2) = g s1
+        in (fab a, s2)
+
+instance Monad PwdOp where
+    return = pure
+    (PwdOp f) >>= k = PwdOp $ \s ->
+        let (a, s1) = f s
+            PwdOp g = k a
+        in g s1
 
 setPassword :: String -> PwdOp ()
--- Your code here
+setPassword newPwd = PwdOp $ \_ -> ((), newPwd)
 
 checkPassword :: String -> PwdOp Bool
--- Your code here
+checkPassword input = PwdOp $ \pwd -> (input == pwd, pwd)
 
 runPwdOp :: PwdOp a -> a
--- Your code here
-
-
-
-
-
+runPwdOp op = fst (runPwdOp' op "")
